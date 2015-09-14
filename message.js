1f3ce0051d582beaa86a8129d0a15fc2b0c11f6c
Ubuntu <singbird81@driverserver.driverserver.m1.internal.cloudapp.net>
2015-09-14 05:44:50 +0000

    add day offset

--- message.js
+++ message.js
@@ -42,8 +42,9 @@ function Message (message) {
                 var day = startDate.getDate()
                 var hour = startDate.getHours() + 8
                 var minutes = startDate.getMinutes()
+                var iDays  =  parseInt(Math.abs(startDate  -  new Date())  /  1000  /  60  /  60  /24)
                 
-                mEvent = mEvent + '時間：' + month + '月' + day + '日' + week[startDate.getDay()] + '  ' + hour + '點' + minutes + '分'
+                mEvent = mEvent + '時間：' + month + '月' + day + '日' + week[startDate.getDay()] + '  ' + hour + '點' + minutes + '分' + '   距離現在： ' + iDays + '天'
                     +' \n地點：'+event.location+' \n事件：'+ event.summary + '\n\n';
             }
             this.mText = mEvent
